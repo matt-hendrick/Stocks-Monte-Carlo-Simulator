ac6d5d30cebf7edbeb6d770f76aca8cd16fe7606
Matt Hendrick <75456865+matt-hendrick@users.noreply.github.com>
2021-05-20 07:06:28 -0400

    Rewrite: Label text for investmentApp

--- investmentApp.py
+++ investmentApp.py
@@ -10,9 +10,9 @@ def runSim():
 st.header("Monte Carlo Simulation of Investment Returns Over Time (1000 simulations)")
 
 symbol = st.text_input("Enter a stock or index to simulate investment OR")
-symbol = st.selectbox(label="You could also select an example ticker from this list", options=[
+symbol = st.selectbox(label="You can also select an example ticker from this list", options=[
                       "^SPX", "^DJI", "^NDQ", "AAPL", "GOOG", "AMZN"])
-st.write("Prominent example tickers: ^SPX = 'S&P 500', ^DIA = 'Dow Jones', ^NDQ = 'Nasdaq Composite AAPL = 'Apple', GOOG = 'Google', AMZN = 'Amazon'")
+st.write("Example tickers: ^SPX = 'S&P 500', ^DIA = 'Dow Jones', ^NDQ = 'Nasdaq Composite AAPL = 'Apple', GOOG = 'Google', AMZN = 'Amazon'")
 
 initialInvestment = st.number_input("Enter your initial investment")
 monthlyInvestment = st.number_input("Enter your monthly investment")
